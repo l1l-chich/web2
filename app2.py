e1674d31801cb1599e3612e15dc80e4d9ae9d54b
alexa <alexey3803@gmail.com>
2026-01-29 01:49:40 +0300

    local web

--- app2.py
+++ app2.py
@@ -1,10 +1,35 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///qwerty.db'
+db = SQLAlchemy(app)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+
+
+    return redirect(url_for('index'))
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/create')
+def create():
+    return render_template('create.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
